5020b9de6aa4447941e3f7a02b79f8b2a722c4f8
BaptisteZloch <bzloch@hotmail.fr>
2024-03-22 12:42:54 +0100

    Refractored API

--- src/main_backend.py
+++ src/main_backend.py
@@ -1,11 +1,6 @@
-import os
-from typing import Dict
+from typing import Dict, Union
 from fastapi.middleware.cors import CORSMiddleware
-
 from fastapi import FastAPI, HTTPException, Depends
-
-import uvicorn
-
 from src.services.pricing_service import PricingService
 from src.utility.schema import (
     BarrierOptionBaseModel,
@@ -21,6 +16,7 @@ from src.utility.schema import (
     StripStrategyBaseModel,
     ZeroCouponBondBaseModel,
 )
+from src.utility.types import BondType, OptionKindType, OptionStrategyType
 
 app = FastAPI()
 
@@ -36,9 +32,10 @@ app.add_middleware(
 )
 
 
-@app.post("/api/v1/price/option/binary")
+@app.post("/api/v1/price/option/{option_kind}")
 def binary_option_pricing(
-    product: BinaryOptionBaseModel,
+    option_kind: OptionKindType,
+    product: Union[BinaryOptionBaseModel, OptionBaseModel, BarrierOptionBaseModel],
     pricing_service: PricingService = Depends(PricingService),
 ) -> Dict[str, float]:
     """Send json like:
@@ -54,145 +51,90 @@ def binary_option_pricing(
     ```
 
     Args:
-        product (BinaryOptionBaseModel): _description_
+        option_kind (OptionKindType): _description_
+        product (Union[BinaryOptionBaseModel, OptionBaseModel, BarrierOptionBaseModel]): _description_
         pricing_service (PricingService, optional): _description_. Defaults to Depends(PricingService).
 
     Raises:
-        HTTPException: Catching all exception
+        ValueError: _description_
+        HTTPException: _description_
 
     Returns:
         Dict[str, float]: _description_
     """
     try:
-        return pricing_service.process_binary_options(product)
+        if option_kind == "binary" and isinstance(product, BinaryOptionBaseModel):
+            return pricing_service.process_binary_options(product)
+        if option_kind == "vanilla" and isinstance(product, OptionBaseModel):
+            return pricing_service.process_vanilla_options(product)
+        if option_kind == "barrier" and isinstance(product, BarrierOptionBaseModel):
+            return pricing_service.process_barrier_options(product)
+        raise ValueError("Provide valid input.")
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"{e}") from e
 
 
-@app.post("/api/v1/price/option/vanilla")
-def vanilla_option_pricing(
-    product: OptionBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    """Send json like :
-    ```json
-    {
-        "spot_price":100,
-        "strike_price": 100,
-        "maturity": 1,
-        "rate": 0.05,
-        "volatility": 0.2,
-        "option_type": "call"
-    }
-    ```
-
-    Args:
-        product (OptionBaseModel): _description_
-        pricing_service (PricingService, optional): _description_. Defaults to Depends(PricingService).
-
-    Raises:
-        HTTPException: Catching all exception
-
-    Returns:
-        Dict[str, float]: _description_
-    """
-    try:
-        return pricing_service.process_vanilla_options(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
+# https://structured-pricing-api-dauphine.koyeb.app/docs
 
 
-@app.post("/api/v1/price/option/barrier")
-def process_barrier_options(
-    product: BarrierOptionBaseModel,
+@app.post("/api/v1/price/option-strategy/{option_strategy}")
+def option_strategy_pricing(
+    option_strategy: OptionStrategyType,
+    product: Union[
+        StraddleStrategyBaseModel,
+        StrangleStrategyBaseModel,
+        ButterflyStrategyBaseModel,
+        CallSpreadStrategyBaseModel,
+        PutSpreadStrategyBaseModel,
+        StripStrategyBaseModel,
+        StrapStrategyBaseModel,
+    ],
     pricing_service: PricingService = Depends(PricingService),
 ) -> Dict[str, float]:
     try:
-        return pricing_service.process_barrier_options(product)
+        assert option_strategy in [
+            "straddle",
+            "strangle",
+            "butterfly",
+            "call-spread",
+            "put-spread",
+            "strip",
+            "strap",
+        ], "Error provide a valid strategy among: straddle, strangle, butterfly, call-spread, put-spread, strip, strap"
+        if option_strategy == "straddle" and isinstance(
+            product, StraddleStrategyBaseModel
+        ):
+            return pricing_service.process_straddle_strategy(product)
+        if option_strategy == "strangle" and isinstance(
+            product, StrangleStrategyBaseModel
+        ):
+            return pricing_service.process_strangle_strategy(product)
+        if option_strategy == "butterfly" and isinstance(
+            product, ButterflyStrategyBaseModel
+        ):
+            return pricing_service.process_butterfly_strategy(product)
+
+        if option_strategy == "call-spread" and isinstance(
+            product, CallSpreadStrategyBaseModel
+        ):
+            return pricing_service.process_call_spread_strategy(product)
+        if option_strategy == "put-spread" and isinstance(
+            product, PutSpreadStrategyBaseModel
+        ):
+            return pricing_service.process_put_spread_strategy(product)
+        if option_strategy == "strip" and isinstance(product, StripStrategyBaseModel):
+            return pricing_service.process_strip_strategy(product)
+        if option_strategy == "strap" and isinstance(product, StrapStrategyBaseModel):
+            return pricing_service.process_strap_strategy(product)
+        raise ValueError("Provide valid input.")
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"{e}") from e
 
 
-@app.post("/api/v1/price/option-strategy/straddle")
-def straddle_strategy_pricing(
-    product: StraddleStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_straddle_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/option-strategy/strangle")
-def strangle_strategy_pricing(
-    product: StrangleStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_strangle_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/option-strategy/butterfly")
-def butterfly_strategy_pricing(
-    product: ButterflyStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_butterfly_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/option-strategy/call-spread")
-def call_spread_strategy_pricing(
-    product: CallSpreadStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_call_spread_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/option-strategy/put-spread")
-def put_spread_strategy_pricing(
-    product: PutSpreadStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_put_spread_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/option-strategy/strip")
-def strip_strategy_pricing(
-    product: StripStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_strip_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/option-strategy/strap")
-def strap_strategy_pricing(
-    product: StrapStrategyBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-) -> Dict[str, float]:
-    try:
-        return pricing_service.process_strap_strategy(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/bond/vanilla")
-def vanilla_bond_pricing(
-    product: BondBaseModel,
+@app.post("/api/v1/price/bond/{bond_type}")
+def bond_pricing(
+    bond_type: BondType,
+    product: Union[BondBaseModel, ZeroCouponBondBaseModel],
     pricing_service: PricingService = Depends(PricingService),
 ):
     """Send json like:
@@ -203,6 +145,13 @@ def vanilla_bond_pricing(
         "nominal":1000,
         "coupon_rate":0.2,
         "nb_coupon":2
+    }
+    ``` or
+    ```json
+    {
+        "rate":0.05,
+        "maturity":1,
+        "nominal":1000
     }
     ```
 
@@ -216,38 +165,17 @@ def vanilla_bond_pricing(
     Returns:
         Dict[str, float]: _description_
     """
+
     try:
-        return pricing_service.process_vanilla_bond(product)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"{e}") from e
-
-
-@app.post("/api/v1/price/bond/zerocoupon")
-def zero_coupon_bond_pricing(
-    product: ZeroCouponBondBaseModel,
-    pricing_service: PricingService = Depends(PricingService),
-):
-    """Send json like :
-    ```json
-    {
-        "rate":0.05,
-        "maturity":1,
-        "nominal":1000
-    }
-    ```
-
-    Args:
-        product (ZeroCouponBondBaseModel): _description_
-        pricing_service (PricingService, optional): _description_. Defaults to Depends(PricingService).
-
-    Raises:
-        HTTPException: Catching all exception
-
-    Returns:
-        Dict[str, float]: _description_
-    """
-    try:
-        return pricing_service.process_zero_coupon_bond(product)
+        assert bond_type in [
+            "vanilla",
+            "zero-coupon",
+        ], "Error provide a valid bond type among: vanilla, zero-coupon"
+        if bond_type == "vanilla" and isinstance(product, BondBaseModel):
+            return pricing_service.process_vanilla_bond(product)
+        if bond_type == "zero-coupon" and isinstance(product, ZeroCouponBondBaseModel):
+            return pricing_service.process_zero_coupon_bond(product)
+        raise ValueError("Provide valid input.")
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"{e}") from e
 
