20314674d26e9d27240ac25cb8b600b98bac8801
Hugo Soleau <aikanaro.al2@gmail.com>
2024-03-23 22:36:09 +0100

    Ajout des certificats de surperformance

--- src/pricing/structured_products.py
+++ src/pricing/structured_products.py
@@ -43,25 +43,87 @@ class ReverseConvertible(StructuredProductBase):
             "put"
             )
         
-        self._price = self.__converse_rate * bond.compute_price() + (1-self.__converse_rate) * option.compute_price()
+        self._price = bond.compute_price() - (1-self.__converse_rate) * option.compute_price()
         
         return self._price
 
 
     def compute_greeks(self) -> Dict[str, float]:
-        raise NotImplementedError()
+        return {
+            "delta": 0.0,
+            "gamma": 0.0,
+            "theta": 0.0,
+            "rho": 0.0,
+            "vega": 0.0,
+        }
 
 
 class OutperformerCertificate(StructuredProductBase):
     
     def __init__(
         self,
-        
+        rate: Rate,
+        maturity: Maturity,
+        nominal: int,
+        spot_price: float,
+        strike_price1: float,
+        strike_price2: float,
+        volatility: Volatility,
+        n_call: float,
     ) -> None:
         super().__init__("outperformer-certificate")
-
+        self.__rate = rate
+        self.__maturity = maturity
+        self.__nominal = nominal
+        self.__spot_price = spot_price
+        self.__strike_price1 = strike_price1
+        self.__strike_price2 = strike_price2
+        self.__volatility = volatility
+        self.__n_call = n_call
+        
+        
     def compute_price(self) -> float:
-        raise NotImplementedError()
+        mat = Maturity(maturity_in_years=1)
+        bond = ZeroCouponBond(
+            self.__rate, 
+            self.__maturity, 
+            self.__nominal
+            )
+        option_call1 = BinaryOption(
+            self.__spot_price, 
+            self.__strike_price1, 
+            mat, 
+            self.__rate, 
+            self.__volatility,
+            "call"
+            )
+        option_call2 = BinaryOption(
+            self.__spot_price, 
+            self.__strike_price2, 
+            mat, 
+            self.__rate, 
+            self.__volatility,
+            "call"
+            )
+        option_put = BinaryOption(
+            self.__spot_price, 
+            self.__strike_price1, 
+            mat, 
+            self.__rate, 
+            self.__volatility,
+            "put"
+            )
+        self._price = bond.compute_price() \
+                    + self.__n_call * option_call1.compute_price() \
+                    - self.__n_call * option_call2.compute_price() \
+                    - option_put.compute_price()
+        return self._price
 
     def compute_greeks(self) -> Dict[str, float]:
-        raise NotImplementedError()
+        return {
+            "delta": 0.0,
+            "gamma": 0.0,
+            "theta": 0.0,
+            "rho": 0.0,
+            "vega": 0.0,
+        }
